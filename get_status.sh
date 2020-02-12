8adec690ff3b5d7818b1bbe1c3b07696b48bbdac
Juri Dispan <juri.dispan@uni-ulm.de>
2020-02-12 16:23:47 +0100

    fixed script

--- get_status.sh
+++ get_status.sh
@@ -5,4 +5,4 @@ JSON_REPLY=$( curl https://sport.uni-ulm.de/unifitstatus --insecure --silent )
 HOUR=$( echo "$JSON_REPLY" | grep -o '[0-9]*' | sed 1q )
 CHECKED_IN=$( echo "$JSON_REPLY" | grep -o '[0-9]*' | sed -n '2{p;q}' )
 
-printf "$( date '+%Y-%m-%d %H:%M:%S') , %02d, %d" "$CHECKED_IN" "$HOUR"
+printf "$( date '+%Y-%m-%d %H:%M:%S') , %02d, %d \n" "$CHECKED_IN" "$HOUR"
