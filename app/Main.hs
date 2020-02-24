2362b9b25867c5bfe9493e5aa85d47691d035c2c
Juri Dispan <juri.dispan@uni-ulm.de>
2020-02-24 13:35:05 +0100

    more data

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,16 @@ module Main where
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend
 import Data.Time.LocalTime
+import Data.Time.Calendar
 
-import Prices(prices,mkDate,filterPrices)
+prices' :: [(LocalTime, Double)]
+prices' = [(mkDateTime 21 10 1997 10 15, 1337)]
 
-prices' :: [(LocalTime,Double,Double)]
-prices' = filterPrices prices (mkDate 1 1 2005) (mkDate 31 12 2006)
+mkDateTime dd mm yyyy hh nn =
+    LocalTime (fromGregorian (fromIntegral yyyy) mm dd)
+              (dayFractionToTimeOfDay ((hh*60+nn)/1440))
 
 main = toFile def "example2_big.png" $ do
     layoutlr_title .= "Price History"
     layoutlr_left_axis . laxis_override .= axisGridHide
-    layoutlr_right_axis . laxis_override .= axisGridHide
     plotLeft (line "price 1" [ [ (d,v) | (d,v,_) <- prices'] ])
-    plotRight (line "price 2" [ [ (d,v) | (d,_,v) <- prices'] ])
