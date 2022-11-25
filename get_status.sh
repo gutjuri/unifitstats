50ba32c7afbbe0300a6b89ac5fed70932c65f294
Juri Dispan <juri.dispan@posteo.net>
2022-11-25 09:47:37 +0100

    Adapt to new website design

--- get_status.sh
+++ get_status.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-JSON_REPLY=$( curl https://sport.uni-ulm.de/unifitstatus --insecure --silent )
+REPLY=$( curl https://sport.uni-ulm.de/de/unifit/information --silent | grep "Checkins seit")
 
-HOUR=$( echo "$JSON_REPLY" | grep -o '[0-9]*' | sed 1q )
-CHECKED_IN=$( echo "$JSON_REPLY" | grep -o '[0-9]*' | sed -n '2{p;q}' )
+CHECKED_IN=$( echo "$REPLY" | grep -o '[0-9]*' | sed -n '2{p;q}' )
+HOUR=$( echo "$REPLY" | grep -o '[0-9]*' | sed -n '3{p;q}' )
 
 printf "$( date '+%Y-%m-%d %H:%M:%S'), %02d, %d \n" "$CHECKED_IN" "$HOUR"
